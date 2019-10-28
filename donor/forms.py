338faf3f220f1e5cf26439a4ed7e8389def2928d
Birat Khadka <biratkhadka11@gmail.com>
2019-10-28 13:15:00 +0545

    Add donor forms and URL configuration

---
+++ donor/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import Bloodreq, Contact, Donor, Payment, Stock
+
+class Donorform(forms.ModelForm):
+    class Meta :
+        model = Donor
+        fields = '__all__'
+    
+class Bloodreqform(forms.ModelForm):
+    class Meta:
+        model = Bloodreq
+        fields = '__all__'
+
+class Stockform(forms.ModelForm):
+    class Meta:
+        model = Stock
+        fields = '__all__'
+    
+class Contactform(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = '__all__'
+
+class Paymentform(forms.ModelForm):
+    class Meta:
+        model = Payment
+        fields = [
+            "donor_name",
+            "email",
+            "phone",
+            "amount",
+            "purpose",
+            "payment_method",
+            "remarks",
+        ]
+
+class createuserform(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
---
+++ donor/urls.py
@@ -0,0 +1,31 @@
+from django.urls import include, path
+from donor import views
+
+urlpatterns = [
+    path('api/', include('donor.api_urls')),
+    path('', views.home , name="home"),
+    path('blog', views.blog , name="blog"),
+    path('about', views.about , name="about"),
+    path('contact', views.contact , name="contact"),
+    path('donor', views.donor , name="donor"),
+    path('deletednr/<int:id>/', views.delete_dnr, name = "deletednr"),
+    path('updatednr/<int:id>/', views.update_dnr, name = "updatednr"),
+    path('updataednr/<int:id>/', views.update_dnr),
+    path('bloodreq', views.bloodreq , name="bloodreq"),
+    path('deletebreq/<int:id>/', views.delete_breq, name = "deletebreq"),
+    path('updatebreq/<int:id>/', views.update_breq, name = "updatebreq"),
+    path('updataebreq/<int:id>/', views.update_breq),
+    path('stock', views.stock , name="stock"),
+    path('deletestk/<int:id>/', views.delete_stk, name = "deletestk"),
+    path('updatestk/<int:id>/', views.update_stk, name = "updatestk"),
+    path('updataestk/<int:id>/', views.update_stk),
+    path('payment', views.payment, name="payment"),
+    path('reports', views.reports, name="reports"),
+    path('reports/live-data', views.live_report_data, name="live_report_data"),
+    path('reports/export/csv', views.export_reports_csv, name="export_reports_csv"),
+    path('terms', views.terms , name="terms"),
+    path('eligible', views.eligible , name="eligible"),
+    path('login', views.login , name = "login" ),
+    path('logout', views.logout , name = "logout" ),
+    path('register', views.register, name = "register" ),
+]
